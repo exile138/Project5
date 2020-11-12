1ca93c1f0639d475ad6b08c5a9657583db306188
Ruslan Kovalchuk <rukovalchuk2@gmail.com>
2020-11-11 23:42:02 -0500

    updating again

--- src/hello.java
+++ src/hello.java
@@ -1,9 +1,5 @@
-package com.company;
-
-public class Main {
-
+public class hello {
     public static void main(String[] args) {
-	// write your code here
 
         System.out.println("hello");
     }
